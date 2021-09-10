40540da0b22828e28f47298019a61d29aa2f0066
aiyu666 <zrobin8741@gmail.com>
2021-09-11 00:04:31 +0800

    Add example code and example unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   env: {
+    'jest/globals': true,
     browser: true,
     es2021: true,
     node: true,
+    webextensions: true,
   },
   extends: [
     'airbnb-base',
   ],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
